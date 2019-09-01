be9c139aba51ef7f96b2bc7f00d433e9dfbbc9f2
Adam Nguyen <nvhadam90@gmail.com>
2019-08-31 21:23:22 -0700

    Added solution to HW 14

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,61 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var table = d3.select('table');
+table.attr('class', 'table table-striped');
+var tbody = d3.select('tbody');
+
+tableData.forEach(function(UFOData) {
+    console.log(UFOData);
+    var row = tbody.append('tr');
+
+    Object.entries(UFOData).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+var button = d3.select('#filter-btn');
+
+button.on("click", function() {
+  var inputElement = d3.select('#datetime');
+  var inputValue = inputElement.property('value');
+  console.log(inputValue);
+  
+  $('#myTableId').empty();
+
+  if (inputValue === "") {
+    
+    tableData.forEach(function(UFOData) {
+      console.log(UFOData);
+      var row = tbody.append('tr');
+
+      Object.entries(UFOData).forEach(function([key, value]) {
+          console.log(key, value);
+          var cell = row.append('td');
+          cell.text(value);
+      });
+    })
+    }
+    else {
+      var filteredData = tableData.filter(occurrence => occurrence.datetime === inputValue);
+      console.log(filteredData);
+
+      // $('#myTableId').empty();
+
+      filteredData.forEach(function(UFOData) {
+        console.log(UFOData);
+        var row = tbody.append('tr');
+
+        Object.entries(UFOData).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+      });
+    }
+  
+});
+
+
